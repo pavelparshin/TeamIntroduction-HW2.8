7aa94b722c91129d7add54ee8563f600ddb938b3
Pavel Parshin <mail@pavelparshin.ru>
2020-06-12 19:20:23 +0300

    Initial Commit

---
+++ TeamIntroduction/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TeamIntroduction
+//
+//  Created by Pavel Parshin on 12.06.2020.
+//  Copyright © 2020 Pavel Parshin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
